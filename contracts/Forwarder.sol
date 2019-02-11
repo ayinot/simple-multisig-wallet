79bc7e4ddf2894e93294c74a422809b1d89d04b7
Terry Wilkinson <tawilkster@gmail.com>
2019-02-11 13:33:10 +0800

    changes made to add ERC20 and update to 0.5.3, still issue with .call.value()()

--- contracts/Forwarder.sol
+++ contracts/Forwarder.sol
@@ -1,46 +1,63 @@
-pragma solidity ^0.4.24;
+pragma solidity ^0.5.3;
 
+import "./IERC20.sol";
 /**
  * Contract that will forward any incoming Ether to the creator of the contract
  */
 contract Forwarder {
-  // Address to which any funds sent to this contract will be forwarded
-  address public parentAddress;
-  event ForwarderDeposited(address from, uint value, bytes data);
+    // Address to which any funds sent to this contract will be forwarded
+    address payable public parentAddress;
+    event ForwarderDeposited(address from, uint value, bytes data);
 
-  /**
-   * Create the contract, and sets the destination address to that of the creator
-   */
-  constructor() public {
-    parentAddress = msg.sender;
-  }
-
-  /**
-   * Modifier that will execute internal code block only if the sender is the parent address
-   */
-  modifier onlyParent {
-    if (msg.sender != parentAddress) {
-      revert();
+    /**
+    * Create the contract, and sets the destination address to that of the creator
+    */
+    constructor() public {
+        parentAddress = msg.sender;
     }
-    _;
-  }
 
-  /**
-   * Default function; Gets called when Ether is deposited, and forwards it to the parent address
-   */
-  function() public payable {
-    // throws on failure
-    parentAddress.transfer(msg.value);
-    // Fire off the deposited event if we can forward it
-    emit ForwarderDeposited(msg.sender, msg.value, msg.data);
-  }
+    /**
+    * Modifier that will execute internal code block only if the sender is the parent address
+    */
+    modifier onlyParent {
+        if (msg.sender != parentAddress) {
+            revert();
+        }
+        _;
+    }
 
-  /**
-   * It is possible that funds were sent to this address before the contract was deployed.
-   * We can flush those funds to the parent address.
-   */
-  function flush() public {
-    // throws on failure
-    parentAddress.transfer(address(this).balance);
-  }
+    /**
+    * Default function; Gets called when Ether is deposited, and forwards it to the parent address
+    */
+    function() external payable {
+        // throws on failure
+        parentAddress.transfer(msg.value);
+        // Fire off the deposited event if we can forward it
+        emit ForwarderDeposited(msg.sender, msg.value, msg.data);
+    }
+
+    /**
+    * Execute a token transfer of the full balance from the forwarder token to the parent address
+    * @param tokenContractAddress the address of the erc20 token contract
+    */
+    function flushTokens(address tokenContractAddress) public onlyParent {
+        IERC20 instance = IERC20(tokenContractAddress);
+        address forwarderAddress = address(this);
+        uint forwarderBalance = instance.balanceOf(forwarderAddress);
+        if (forwarderBalance == 0) {
+            return;
+        }
+        if (!instance.transfer(parentAddress, forwarderBalance)) {
+            revert();
+        }
+    }
+
+    /**
+    * It is possible that funds were sent to this address before the contract was deployed.
+    * We can flush those funds to the parent address.
+    */
+    function flush() public {
+        // throws on failure
+        parentAddress.transfer(address(this).balance);
+    }
 }
